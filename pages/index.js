458ae5b729f9cbae2fe6f4f66cb6b07052b60ff4
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2021-05-20 15:24:50 -0300

    Overflow hidden stack index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import { Grid, GridItem } from '@chakra-ui/layout';
+import { Stack } from '@chakra-ui/react';
 import React from 'react';
 import { Navbar, About, Contact, Landing, Projects } from '../components';
 
 export default function Home() {
   return (
-    <>
+    <Stack overflowX="hidden">
       <Navbar />
       <Landing />
       <Projects />
-    </>
+    </Stack>
   );
 }
