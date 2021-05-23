170e22e0535f94af11962e2b3278ee269062dab2
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2021-05-23 03:56:32 -0300

    About, separate components

--- client/components/About/About.js
+++ client/components/About/About.js
@@ -21,49 +21,15 @@ export function About() {
       justifyContent="center"
       alignItems="center"
     >
-      <Box
-        className="about__eclipse"
-        display={{
-          base: 'none',
-          sm: 'block',
-          md: 'block',
-          lg: 'block',
-        }}
-        position="absolute"
-        top="-40%"
-        right="30%"
-        width="2450px"
-        height="2450px"
-        borderRadius="50%"
-        zIndex={-1}
-      />
+      <EclipseOne />
       <Stack
         justifyContent="center"
         alignItems="center"
         className="about__title"
         marginBottom={100}
       >
-        <VStack>
-          <Text
-            as={'h1'}
-            fontSize={{ base: '4rem', sm: '5rem', md: '8rem', lg: '11.642rem' }}
-            marginBottom={8}
-          >
-            About
-          </Text>
-        </VStack>
-        <VStack
-          textAlign="center"
-          w={{ base: '80%', sm: '60%', md: '80&', lg: '80%' }}
-        >
-          <Text className="about__title__presentation" marginBottom={4}>
-            Hi, I'm Rodrigo, a Fullstack Developer based in Argentina.
-          </Text>
-          <Text className="about__title__presentation">
-            Passionate about Software and Web development. I love to learn new
-            things as well as solving real-world problems.
-          </Text>
-        </VStack>
+        <Title>About</Title>
+        <Subtitle />
       </Stack>
       <Grid
         position="relative"
@@ -73,116 +39,192 @@ export function About() {
         templateColumns="repeat(12,1fr)"
         gap={4}
       >
-        <GridItem
-          colStart={{ base: 2, sm: 2, md: 3, lg: 3 }}
-          colEnd={{ base: 12, sm: 12, md: 11, lg: 8 }}
-          marginBottom={16}
-        >
-          <Stack justifyContent="center" alignItems="center">
-            <Image
-              src={'/goals-image1.jpg'}
-              alt="goal image"
-              h={'auto'}
-              borderRadius="20px"
-            />
-          </Stack>
-
-          <Stack
-            textAlign={{ sm: 'left', md: 'left', lg: 'right' }}
-            padding={4}
-            marginTop={6}
-            className="about__goal__first-text"
-          >
-            <Text>
-              My goal is to provide high-quality work to clients no matter if
-              the project is small or of high complexity.
-            </Text>
-            <Text>
-              I entered the world of Software development because I realized
-              that I can make an impact on people by just using my imagination
-              and passion when building applications.
-            </Text>
-          </Stack>
-        </GridItem>
-        <GridItem
-          position="absolute"
-          top={{ md: '30%', lg: '30%' }}
-          display={{ base: 'none', md: 'block' }}
-          colStart={{ md: 9, lg: 8 }}
-          zIndex={-1}
-        >
-          <Stack>
-            <Text
-              as={'h1'}
-              fontSize={{
-                base: '8rem',
-                sm: '10rem',
-                md: '10rem',
-                lg: '14.552rem',
-              }}
-              className="about__goal__title"
-            >
-              Goal
-            </Text>
-          </Stack>
-        </GridItem>
-        <GridItem
-          colStart={{ base: 2, sm: 2, md: 3, lg: 3 }}
-          colEnd={{ base: 12, sm: 12, md: 11, lg: 11 }}
-        >
-          <Stack
-            justifyContent="center"
-            alignItems="center"
-            direction={{
-              base: 'column',
-              sm: 'column',
-              md: 'column',
-              lg: 'row',
-            }}
-            spacing={10}
-          >
-            <Image
-              src={'/goals-image2.jpg'}
-              alt="goal image"
-              h={'auto'}
-              borderRadius="20px"
-            />
-            <Stack className="about__goal__second-text">
-              <Text>
-                By 2018, I thought it would be a good idea to start
-                documenting/blogging my journey of becoming a developer to
-                inspire others as well as keep myself accountable. So I started
-                my Instagram{' '}
-                <Link
-                  href="https://www.instagram.com/rt.codes/"
-                  isExternal
-                  className="about__goal__second-text__link"
-                >
-                  @rt.codes
-                </Link>{' '}
-                talking about the progress I was making and what concepts I was
-                currently learning.
-              </Text>
-              <Text>
-                I also did a bunch of Personal projects with the knowledge I
-                gathered from courses and the Internet. Check them out on the
-                button below.
-              </Text>
-            </Stack>
-          </Stack>
-        </GridItem>
+        <FirstGoal />
+        <GoalTitle />
+        <SecondGoal />
       </Grid>
-      <Stack
-        justifyContent="center"
-        alignItems="center"
-        className="about__button"
-      >
-        <NextLink href="/#projects">
-          <Button colorScheme={'teal'} paddingX={10}>
-            View Projects
-          </Button>
-        </NextLink>
-      </Stack>
+
+      <ButtonViewProject>View Projects</ButtonViewProject>
     </Stack>
   );
 }
+
+const ButtonViewProject = ({ children }) => {
+  return (
+    <Stack
+      justifyContent="center"
+      alignItems="center"
+      className="about__button"
+    >
+      <NextLink href="/#projects">
+        <Button colorScheme={'teal'} paddingX={10}>
+          {children}
+        </Button>
+      </NextLink>
+    </Stack>
+  );
+};
+
+const Title = ({ children }) => {
+  return (
+    <VStack>
+      <Text
+        as={'h1'}
+        fontSize={{ base: '4rem', sm: '5rem', md: '8rem', lg: '11.642rem' }}
+        marginBottom={8}
+      >
+        {children}
+      </Text>
+    </VStack>
+  );
+};
+
+const Subtitle = () => {
+  return (
+    <VStack
+      textAlign="center"
+      w={{ base: '80%', sm: '60%', md: '80&', lg: '80%' }}
+    >
+      <Text className="about__title__presentation" marginBottom={4}>
+        Hi, I'm Rodrigo, a Fullstack Developer based in Argentina.
+      </Text>
+      <Text className="about__title__presentation">
+        Passionate about Software and Web development. I love to learn new
+        things as well as solving real-world problems.
+      </Text>
+    </VStack>
+  );
+};
+
+const GoalTitle = () => {
+  return (
+    <GridItem
+      position="absolute"
+      top={{ md: '30%', lg: '30%' }}
+      display={{ base: 'none', md: 'block' }}
+      colStart={{ md: 9, lg: 8 }}
+      zIndex={-1}
+    >
+      <Stack>
+        <Text
+          as={'h1'}
+          fontSize={{
+            base: '8rem',
+            sm: '10rem',
+            md: '10rem',
+            lg: '14.552rem',
+          }}
+          className="about__goal__title"
+        >
+          Goal
+        </Text>
+      </Stack>
+    </GridItem>
+  );
+};
+
+const FirstGoal = () => {
+  return (
+    <GridItem
+      colStart={{ base: 2, sm: 2, md: 3, lg: 3 }}
+      colEnd={{ base: 12, sm: 12, md: 11, lg: 8 }}
+      marginBottom={16}
+    >
+      <Stack justifyContent="center" alignItems="center">
+        <Image
+          src={'/goals-image1.jpg'}
+          alt="goal image"
+          h={'auto'}
+          borderRadius="20px"
+        />
+      </Stack>
+
+      <Stack
+        textAlign={{ sm: 'left', md: 'left', lg: 'right' }}
+        padding={4}
+        marginTop={6}
+        className="about__goal__first-text"
+      >
+        <Text>
+          My goal is to provide high-quality work to clients no matter if the
+          project is small or of high complexity.
+        </Text>
+        <Text>
+          I entered the world of Software development because I realized that I
+          can make an impact on people by just using my imagination and passion
+          when building applications.
+        </Text>
+      </Stack>
+    </GridItem>
+  );
+};
+
+const SecondGoal = () => {
+  return (
+    <GridItem
+      colStart={{ base: 2, sm: 2, md: 3, lg: 3 }}
+      colEnd={{ base: 12, sm: 12, md: 11, lg: 11 }}
+    >
+      <Stack
+        justifyContent="center"
+        alignItems="center"
+        direction={{
+          base: 'column',
+          sm: 'column',
+          md: 'column',
+          lg: 'row',
+        }}
+        spacing={10}
+      >
+        <Image
+          src={'/goals-image2.jpg'}
+          alt="goal image"
+          h={'auto'}
+          borderRadius="20px"
+        />
+        <Stack className="about__goal__second-text">
+          <Text>
+            By 2018, I thought it would be a good idea to start
+            documenting/blogging my journey of becoming a developer to inspire
+            others as well as keep myself accountable. So I started my Instagram{' '}
+            <Link
+              href="https://www.instagram.com/rt.codes/"
+              isExternal
+              className="about__goal__second-text__link"
+            >
+              @rt.codes
+            </Link>{' '}
+            talking about the progress I was making and what concepts I was
+            currently learning.
+          </Text>
+          <Text>
+            I also did a bunch of Personal projects with the knowledge I
+            gathered from courses and the Internet. Check them out on the button
+            below.
+          </Text>
+        </Stack>
+      </Stack>
+    </GridItem>
+  );
+};
+
+const EclipseOne = () => {
+  return (
+    <Box
+      className="about__eclipse"
+      display={{
+        base: 'none',
+        sm: 'block',
+        md: 'block',
+        lg: 'block',
+      }}
+      position="absolute"
+      top="-40%"
+      right="30%"
+      width="2450px"
+      height="2450px"
+      borderRadius="50%"
+      zIndex={-1}
+    />
+  );
+};
