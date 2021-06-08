948405c93db9dffd7dcdd330797f4ce23b1f3b86
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2021-06-08 17:03:27 -0300

    Head meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,19 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta
+            property="og:title"
+            content="Rodrigo Toyama - Fullstack Developer"
+          />
+          <meta
+            property="og:description"
+            content="Hi, I'm Rodrigo, a Fullstack Developer based in Argentina."
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://toyamarodrigo.com/" />
+          <meta property="og:image" content="https://i.imgur.com/k5RZyrk.png" />
+        </Head>
         <body>
           <ColorModeScript />
           <Main />
